f187fb5241536c0affe3a2db53eb7c1d2d06fb6a
Umang Devanshu <81811004+devumang096@users.noreply.github.com>
2023-05-02 23:23:54 +0530

    Update script.js

--- login admin/script.js
+++ login admin/script.js
@@ -2,7 +2,7 @@ function login() {
     const id = document.getElementById("id").value;
     const password = document.getElementById("password").value;
   
-    if (id === "admin" && password === "password") {
+    if (id === "admin@gmail.com" && password === "password") {
       window.location.href = "./../admin dashboard/index.html";
     } else {
       document.getElementById("output").innerHTML = "<p id='error'>Invalid ID or Password</p>";
